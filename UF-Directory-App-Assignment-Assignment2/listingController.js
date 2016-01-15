768898166da6530a70d0222363e1e5e4359d7dc2
Collin Irwin <irwincollin+github@gmail.com>
2016-01-14 23:27:19 -0500

    Now possible to add listings using a form. Fixed bug with index mismatch during search
    Also added title

--- UF-Directory-App-Assignment-Assignment2/listingController.js
+++ UF-Directory-App-Assignment-Assignment2/listingController.js
@@ -2,12 +2,28 @@ angular.module('listings').controller('ListingsController', ['$scope', 'Listings
   function($scope, Listings) {
     $scope.listings = Listings;
     $scope.detailedInfo = undefined;
-    $scope.newListing = {}
+    $scope.newListing = {
+      coordinates: {}
+    };
 
-    $scope.addListing = function() {}; /* TODO */
+    $scope.addListing = function() {
+      var newListingToInsert = {
+        name: $scope.newListing.name,
+        code: $scope.newListing.code,
+        address: $scope.newListing.address,
+        coordinates: {
+          latitude: $scope.newListing.coordinates.latitude,
+          longitude: $scope.newListing.coordinates.longitude
+        }
+      }
+      $scope.listings.push(newListingToInsert);
+      clearForm();
+    };
+
     $scope.deleteListing = function(index) {
       $scope.listings.splice(index, 1);
     };
+
     $scope.showDetails = function(index) {
       $scope.detailedInfo = $scope.listings[index];
       $scope.detailedInfo.showDetails = true
